3bcddf1327382e10ea16371ebdc081f0d4cc38af
pen <pen@luckpen.com>
2023-09-17 23:04:42 +0800

    将分类器预测的实体类型，应用于test2.json 测试效果

--- data/ner2/process2.py
+++ data/ner2/process2.py
@@ -1,6 +1,7 @@
 import json
 
-data = json.load(open("./test.json", encoding="utf-8"))["data"]
+# 0917 train2.json 在原有json的基础上添加ent_types内容
+data = json.load(open("./train.json", encoding="utf-8"))["data"]
 
 new_data = []
 for example in data:
@@ -13,5 +14,22 @@ for example in data:
     example["ent_types"] = ent_types
     new_data.append(example)
 
-# 0917 train2.json 在原有json的基础上添加ent_types内容，test2.json 中ent_types的内容来自分类器
 json.dump({"data": new_data}, open("./train2.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+
+# 0917 test2.json 中ent_types的内容来自分类器
+with open(r'predict_results_None.txt') as f:
+    lines = f.readlines()
+    all_pred = []
+    for i in range(1, len(lines)):
+        pred_types = json.loads(lines[i])['pred']
+        all_pred.append(pred_types)
+
+    data = json.load(open("./test.json", encoding="utf-8"))["data"]
+    new_data = []
+
+    for i in range(len(data)):
+        example = data[i]
+        example["ent_types"] = all_pred[i]
+        new_data.append(example)
+
+    json.dump({"data": new_data}, open("./test2.json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
